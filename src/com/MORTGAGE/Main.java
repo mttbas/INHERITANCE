01ccb31b9924329e2619f1458166a7f32ed93212
Maryam <m.moh@gmx.ch>
2021-02-16 14:58:17 +0100

    Initial commit

---
+++ src/com/MORTGAGE/Main.java
@@ -0,0 +1,10 @@
+package com.MORTGAGE;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var control = new UIControl();
+        control.disable();
+        System.out.println(control.isEnabled());
+    }
+}
