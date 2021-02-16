19d80809144878e9c4542f6ac6f65e30904cb6e1
Maryam <m.moh@gmx.ch>
2021-02-16 15:34:38 +0100

    "extends UIControl" is used to inherit all the features in UIControl class

--- src/com/MORTGAGE/Main.java
+++ src/com/MORTGAGE/Main.java
@@ -3,8 +3,10 @@ package com.MORTGAGE;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new UIControl();
+        var control = new TextBox();
         control.disable();
+        control.setText("see how we inherited the features in UIControl class");
         System.out.println(control.isEnabled());
+        System.out.println(control.getText()); // to print it out we need a getter which I created beforehand.
     }
 }
---
+++ src/com/MORTGAGE/TextBox.java
@@ -0,0 +1,32 @@
+package com.MORTGAGE;
+
+
+/*
+* to have this class inherit all the features that we implemented in UI Control class we simply type:
+* extends UIControl after class NAME
+* if you go in main class and replace UIControl with TextBox, this OBJECT will will have 5 methods
+* 2 methods in TextBox class + 3 methods in UIControl class
+* just type the control object name with dot operator to see the members
+ */
+
+public class TextBox extends UIControl {
+    /*
+    *we have 1 FIELD
+    *at first it was : public String text; but if we do not initialize it we may get problem
+    *when creating OBJECTS. see the EXAMPLE OBJECTS
+     */
+    private String text = "";
+
+
+    // and 2 methods
+    public void setText(String text){
+        this.text = text;
+    }
+    public void clear() {
+        text = "";
+    }
+
+    public String getText() {
+        return text;
+    }
+}
