bb115b94ce173d1ab9e9ebca39cebbb62e2cb2dd
Maryam <m.moh@gmx.ch>
2021-02-16 22:09:41 +0100

    all classes inherits from the OBJECT CLASS. we do not see "extends Object" but we have all the features, methods and fields in this class

--- src/com/MORTGAGE/Main.java
+++ src/com/MORTGAGE/Main.java
@@ -3,10 +3,20 @@ package com.MORTGAGE;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new TextBox();
-        control.disable();
-        control.setText("see how we inherited the features in UIControl class");
-        System.out.println(control.isEnabled());
-        System.out.println(control.getText()); // to print it out we need a getter which I created beforehand.
+        var obj = new Object();
+
+        /* any class that we declare directly or indirectly inherits from the OBJECT CLASS and so
+        * it has all the features of Object class; all the methods and fields.
+        * here TextBox class extends UIControl and it is visible.
+        * but any class we create in fact extends Object (class) also and has all the behaviours
+        * if we type obj. here we see lots of those methods.
+        *
+        * obj.getClass(); // we can read the metadata of an object
+        * obj.hashCode(); //returns an integer based on the address of this object in memory
+        * obj.toString(); // returns the string representation of an object
+        * obj.equals(textBox); // used for comparing objects
+        * obj.notify(); // used in concurrency - advanced topic
+        * obj.notifyAll(); // // used in concurrency - advanced topic
+         */
     }
 }
